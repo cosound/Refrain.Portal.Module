42c2bde7f5c671767b531590062a5a17487ccb4c
Jesper Fyhr Knudsen <Jesper@fyhr.dk>
2014-04-25 18:27:22 +0200

    Added similarity data to SongViewData

--- src/app/Refrain.Portal.Module/View/SongView.cs
+++ src/app/Refrain.Portal.Module/View/SongView.cs
@@ -2,7 +2,9 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Linq;
+    using System.Xml.Linq;
     using CHAOS.Serialization;
     using Chaos.Portal.Core.Indexing.View;
     using Object = Chaos.Mcm.Data.Dto.Object;
@@ -28,12 +30,48 @@
 
         private IEnumerable<IViewData> Index(Object manifest)
         {
+            var similarityXml = manifest.Metadatas.FirstOrDefault(item => item.MetadataSchemaGuid == Config.MetadataSchemas.GenericSimilarity);
 
+            if (similarityXml == null || similarityXml.MetadataXml.Root == null) yield break;
 
-            yield return new SongViewData
+            foreach (var similarityElement in similarityXml.MetadataXml.Root.Elements("Similarity"))
+            {
+                var basicsElement = similarityElement.Element("Basics");
+              
+                if (basicsElement == null) continue;
+
+                var identifier = basicsElement.Descendants("Identifier").FirstOrDefault();
+
+                if (identifier == null) continue;
+
+                var songSimilarities = new List<SongSimilarity>();
+
+                foreach (var pointElement in similarityElement.Descendants("EndPoints").Descendants("Point"))
                 {
-                    Id = manifest.Guid
+                    var guid = pointElement.Element("GUID").Value;
+                    var rank = pointElement.Element("Rank").Value;
+                    var dist = pointElement.Element("Dist").Value;
+                    var relativeImportance = pointElement.Element("RelativeImportanceOfSubspaces").Value;
+
+                    songSimilarities.Add(new SongSimilarity
+                        {
+                            SongId = new Guid(guid),
+                            Rank = uint.Parse(rank),
+                            Distance = double.Parse(dist, CultureInfo.InvariantCulture),
+                            RelativeImportance = relativeImportance
+                        });
+                }
+
+                yield return new SongViewData
+                {
+                    Id = manifest.Guid,
+                    Similarity = new Similarity
+                        {
+                            Type = identifier.Value,
+                            Songs = songSimilarities
+                        }
                 };
+            }
         }
     }
 
@@ -42,6 +80,14 @@
         [Serialize]
         public Guid Id { get; set; }
 
+        [Serialize]
+        public Similarity Similarity { get; set; }
+
+        public SongViewData()
+        {
+            Similarity = new Similarity();
+        }
+
         public IEnumerable<KeyValuePair<string, string>> GetIndexableFields()
         {
             throw new System.NotImplementedException();
@@ -49,5 +95,34 @@
 
         public KeyValuePair<string, string> UniqueIdentifier { get { return new KeyValuePair<string, string>("Id", Id.ToString()); } }
         public string Fullname { get { return "Refrain.Portal.Module.Test.View.SongViewData"; } }
+    }
+
+    public class Similarity
+    {
+        [Serialize]
+        public string Type { get; set; }
+
+        [Serialize]
+        public IList<SongSimilarity> Songs { get; set; }
+
+        public Similarity()
+        {
+            Songs = new List<SongSimilarity>();
+        }
+    }
+
+    public class SongSimilarity
+    {
+        [Serialize]
+        public Guid SongId { get; set; }
+
+        [Serialize]
+        public uint Rank { get; set; }
+
+        [Serialize]
+        public double Distance { get; set; }
+
+        [Serialize]
+        public string RelativeImportance { get; set; }
     }
 }
--- src/test/Refrain.Portal.Module.Test/View/SongViewTest.cs
+++ src/test/Refrain.Portal.Module.Test/View/SongViewTest.cs
@@ -17,6 +17,11 @@
 
             Assert.That(result, Is.Not.Null);
             Assert.That(result.Id, Is.EqualTo(new Guid("bb8dd249-c192-405b-8a86-b3fb5ca49819")));
+            Assert.That(result.Similarity.Type, Is.EqualTo("000001"));
+            Assert.That(result.Similarity.Songs[0].SongId, Is.EqualTo(new Guid("fe5ce389-1581-4fe3-91cf-bdcc465d68d0")));
+            Assert.That(result.Similarity.Songs[0].Rank, Is.EqualTo(1));
+            Assert.That(result.Similarity.Songs[0].Distance, Is.EqualTo(0.06));
+            Assert.That(result.Similarity.Songs[0].RelativeImportance, Is.EqualTo("0 0 0 0 0 0.02"));
         }
 
         private SongView Make_SongView()
@@ -24,6 +29,4 @@
             return new SongView(Make_Config());
         }
     }
-
-    
 }
