1c2b0733b4c01a8beebfb781c0d8a9963fc4279a
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2014-05-07 16:28:20 +0100

    Added ability to get TwitterMood with before and after dates. Also updated Portal, with functionality to use paging with groups

--- src/app/Refrain.Portal.Module/Extension/TwitterMood.cs
+++ src/app/Refrain.Portal.Module/Extension/TwitterMood.cs
@@ -1,5 +1,6 @@
 ﻿namespace Refrain.Portal.Module.Extension
 {
+    using System;
     using System.Linq;
     using Chaos.Portal.Core;
     using Chaos.Portal.Core.Data.Model;
@@ -13,22 +14,41 @@
             
         }
 
-        public IGroupedResult<IResult> Get(string country)
+        public IGroupedResult<IResult> Get(string country, DateTime? before, DateTime? after, uint groupPageSize = 1, uint groupPageIndex = 0)
         {
             var moodView = PortalApplication.ViewManager.GetView("TwitterMood");
 
             var moodQuery = new SolrQuery
                 {
-                    Query = GenerateQuery(country), Sort = "DateCreated desc", Group = new SolrGroup
+                    Query = GenerateQuery(country), 
+                    Sort = "DateCreated desc", 
+                    Filter = GenerateFilter(before, after),
+                    PageSize = 100,
+                    Group = new SolrGroup
                         {
-                            Field = "Country.Name", Limit = 1
-                        },
-                    PageSize = 100
+                            Field = "Country.Name",
+                            Limit = groupPageSize,
+                            Offset = groupPageIndex * groupPageSize
+                        }
                 };
 
             var moodResult = moodView.GroupedQuery(moodQuery);
 
             return moodResult;
+        }
+
+        private static string GenerateFilter(DateTime? before, DateTime? after)
+        {
+            if(before.HasValue && after.HasValue)
+                return string.Format("DateCreated:[{0} TO {1}]", after.Value.ToString("yyyy-MM-dd'T'HH:mm:ss'Z'"), before.Value.ToString("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+
+            if (before.HasValue)
+                return string.Format("DateCreated:[* TO {0}]", before.Value.ToString("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+            
+            if (after.HasValue)
+                return string.Format("DateCreated:[{0} TO *]", after.Value.ToString("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+            
+            return "";
         }
 
         private static string GenerateQuery(string country)
--- src/test/Refrain.Portal.Module.Test/Extension/TwitterMoodTest.cs
+++ src/test/Refrain.Portal.Module.Test/Extension/TwitterMoodTest.cs
@@ -1,5 +1,6 @@
 ﻿namespace Refrain.Portal.Module.Test.Extension
 {
+    using System;
     using Chaos.Portal.Core.Indexing;
     using Module.Extension;
     using Moq;
@@ -15,7 +16,7 @@
             var extension = new TwitterMood(PortalApplication.Object);
             var country = "";
 
-            extension.Get(country);
+            extension.Get(country, null, null);
 
             View.Verify(m => m.GroupedQuery(It.Is<IQuery>(item =>
                 item.Query == "*:*" &&
@@ -30,7 +31,7 @@
             var extension = new TwitterMood(PortalApplication.Object);
             var country = "country";
 
-            extension.Get(country);
+            extension.Get(country, null, null);
 
             View.Verify(m => m.GroupedQuery(It.Is<IQuery>(item =>
                 item.Query == "Country.Name:country" &&
@@ -45,10 +46,62 @@
             var extension = new TwitterMood(PortalApplication.Object);
             var country = "country1,country2, country3";
 
-            extension.Get(country);
+            extension.Get(country, null, null);
 
             View.Verify(m => m.GroupedQuery(It.Is<IQuery>(item =>
                 item.Query == "Country.Name:country1 country2 country3" &&
+                item.Sort == "DateCreated desc" &&
+                item.Group.Field == "Country.Name")));
+        }
+        
+        [Test]
+        public void Get_GivenBeforeDateTime_GetTheMoodStateAtThatTime()
+        {
+            PortalApplication.Setup(m => m.ViewManager.GetView("TwitterMood")).Returns(View.Object);
+            var extension = new TwitterMood(PortalApplication.Object);
+            var country = "";
+            var before = new DateTime(2000, 01, 01, 17, 00, 00);
+
+            extension.Get(country, before, null);
+
+            View.Verify(m => m.GroupedQuery(It.Is<IQuery>(item =>
+                item.Query == "*:*" &&
+                item.Filter == "DateCreated:[* TO 2000-01-01T17:00:00Z]" &&
+                item.Sort == "DateCreated desc" &&
+                item.Group.Field == "Country.Name")));
+        }
+        
+        [Test]
+        public void Get_GivenBeforeAndAfterDateTime_GetTheMoodStateBetweenDates()
+        {
+            PortalApplication.Setup(m => m.ViewManager.GetView("TwitterMood")).Returns(View.Object);
+            var extension = new TwitterMood(PortalApplication.Object);
+            var country = "";
+            var before = new DateTime(2001, 01, 01, 17, 00, 00);
+            var after = new DateTime(2000, 01, 01, 17, 00, 00);
+
+            extension.Get(country, before, after);
+
+            View.Verify(m => m.GroupedQuery(It.Is<IQuery>(item =>
+                item.Query == "*:*" &&
+                item.Filter == "DateCreated:[2000-01-01T17:00:00Z TO 2001-01-01T17:00:00Z]" &&
+                item.Sort == "DateCreated desc" &&
+                item.Group.Field == "Country.Name")));
+        }
+        
+        [Test]
+        public void Get_GivenAfterDateTime_GetTheMoodStateBetweenNowAndTheAfterDate()
+        {
+            PortalApplication.Setup(m => m.ViewManager.GetView("TwitterMood")).Returns(View.Object);
+            var extension = new TwitterMood(PortalApplication.Object);
+            var country = "";
+            var after = new DateTime(2000, 01, 01, 17, 00, 00);
+
+            extension.Get(country, null, after);
+
+            View.Verify(m => m.GroupedQuery(It.Is<IQuery>(item =>
+                item.Query == "*:*" &&
+                item.Filter == "DateCreated:[2000-01-01T17:00:00Z TO *]" &&
                 item.Sort == "DateCreated desc" &&
                 item.Group.Field == "Country.Name")));
         }
